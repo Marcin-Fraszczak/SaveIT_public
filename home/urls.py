4b3519190f21267cb7f982c20a8e6885df84ab1d
Marcin-Fraszczak <kolothomasa@wp.pl>
2022-12-04 18:14:25 +0100

    landing page, menu, footer and base template

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'home'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home_view'),
+    # path('about/', views.AboutView.as_view(), name='about_view'),
+    # path('contact/', views.ContactView.as_view(), name='contact_view'),
+    # path('author/', views.AuthorView.as_view(), name='author_view'),
+    # path('tools/', views.ToolsView.as_view(), name='tools_view'),
+    # path('notfound/', views.NotFoundView.as_view(), name='not_found_view'),
+]
