acc7649320b925cb02aa2b230b0a4bb2c6350e65
Marcin-Fraszczak <kolothomasa@wp.pl>
2022-12-04 18:49:54 +0100

    changed name of home view

--- 01_overview/collected_urls.py
+++ 01_overview/collected_urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home_view'),
+    path('', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about_view'),
     path('contact/', views.ContactView.as_view(), name='contact_view'),
     path('author/', views.AuthorView.as_view(), name='author_view'),
