4129db0ef2c6bf40d2f55d62ab4cd09cc305ed62
Marcin-Fraszczak <kolothomasa@wp.pl>
2022-12-15 21:40:59 +0100

    hosts

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("DJ_SECRET_KEY")
 DEBUG = int(os.environ.get('DJ_DEBUG', default=0))
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ["nameless-bayou-17526.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["save-it-project.herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
