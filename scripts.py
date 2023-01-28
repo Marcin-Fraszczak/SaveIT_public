6797e406b4e9187f43f1459f6787b869f6839a80
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-01-28 17:28:40 +0100

    script populating database test version

--- scripts.py
+++ scripts.py
@@ -1,18 +1,9 @@
 """
 Scripts populating database with random data
 """
+import os
 from random import randint, choice, choices
 from datetime import datetime, timedelta
-
-from django.contrib.auth import get_user_model
-
-from transactions.models import Transaction
-from categories.models import Category
-from counterparties.models import Counterparty
-from wallets.models import Wallet
-
-import os
-from random import choices, randint
 
 import django
 from django.contrib.auth import get_user_model
@@ -63,8 +54,101 @@ def create_user(admin=False):
     return user, password
 
 
-def create_categories():
-    pass
+def create_categories(user):
+    object_list = ["work", "fun", "food", "transportation", "sport", "housing", "health"]
+    for obj in object_list:
+        Category.objects.create(name=obj, unique_name=f'{user.username}_{obj}', owner=user)
+        print(obj)
+
+
+def create_counterparties(user):
+    object_list = ["work", "Biedronka", "LIDL", "Tesco", "Post", "TM", "Lufthansa", "PGNiG"]
+    for obj in object_list:
+        Counterparty.objects.create(name=obj, unique_name=f'{user.username}_{obj}', owner=user)
+        print(obj)
+
+
+def create_wallets(user):
+    object_list = ["Personal", "Company"]
+    for obj in object_list:
+        Wallet.objects.create(
+            name=obj,
+            unique_name=f'{user.username}_{obj}',
+            owner=user,
+            is_default=(obj == "Personal")
+        )
+        print(obj)
+
+
+def create_plans(user):
+    object_dict = {
+        "Minimalist": {"goal": 4000, "init": 1000, "curve": 1},
+        "Hedonist": {"goal": 6000, "init": 1200, "curve": 2},
+        "Like No Tomorrow": {"goal": 12000, "init": 4000, "curve": 3},
+    }
+    for obj in object_dict:
+        SavingsPlan.objects.create(
+            name=obj,
+            unique_name=f'{user.username}_{obj}',
+            owner=user,
+            is_default=(obj == "Minimalist"),
+            monthly_goal=object_dict[obj]["goal"],
+            initial_value=object_dict[obj]["init"],
+            curve_type=object_dict[obj]["curve"],
+        )
+        print(obj)
+
+
+def create_transactions(user):
+    def get_days_till_today(start):
+        stop = datetime.now().date()
+        delta = (stop - start).days
+        return delta
+
+    starting_date = datetime(year=2022, month=6, day=1).date()
+    _category = Category.objects.filter(owner=user)
+    _counterparty = Counterparty.objects.filter(owner=user)
+    _wallet = Wallet.objects.filter(owner=user)
+
+    for i in range(get_days_till_today(starting_date)):
+
+        today = starting_date + timedelta(days=i)
+
+        if today.day == 10:
+            transaction = Transaction(
+                counterparty=Counterparty.objects.get(name="WORK"),
+                category=Category.objects.get(name="WORK"),
+                is_profit=True,
+                value=6000,
+                date=today,
+                owner=user,
+            )
+            transaction.save()
+            transaction.wallet.add(choice(_wallet))
+            transaction.save()
+
+        trans_no = randint(0, 3)
+        if not trans_no:
+            continue
+
+        for t in range(trans_no):
+
+            is_profit = choices([False, True], weights=[9, 1])[0]
+            value = randint(5, 200)
+            if not is_profit:
+                value = -value
+
+            transaction = Transaction(
+                counterparty=choice(_counterparty),
+                category=choice(_category),
+                is_profit=is_profit,
+                value=value,
+                date=today,
+                owner=user,
+            )
+            transaction.save()
+            transaction.wallet.add(choice(_wallet))
+            transaction.save()
 
 
 def populate():
@@ -82,97 +166,21 @@ def populate():
     print(f'email: \t {superuser.email}')
     print(f'=' * 60)
     print("Created categories:")
-
+    create_categories(user)
     print(f'=' * 60)
-    print("Created institutions:")
-
+    print("Created counterparties:")
+    create_counterparties(user)
     print(f'=' * 60)
-
-
-
-def populate_db():
-    usernames = ["John", "Derek", "Maria", "Dude"]
-    counterparties = ["Praca", "Enea", "Biedronka", "LIDL", "Auchan", "Żabka", "PKP", "Netia", "policja", "allegro"]
-    categories = ["Praca", "Rozrywka", "Spożywka", "Chemia", "Sport", "Zdrowie"]
-    wallets = ["Osobisty", "Rodzinny", "Firmowy"]
-
-    def get_days_till_today(start):
-        stop = datetime.now().date()
-        delta = (stop - start).days
-        return delta
-
-    for username in usernames[:1]:
-        # user creation
-        user = get_user_model()(username=username, email=f"{username.split()[0]}@gmail.com")
-        user.set_password('Testpass123')
-        user.save()
-        print(f"User {username} created.")
-
-        # all other objects created for this user
-        w_name = wallets[0].upper()
-        w_unique_name = f"{user.username}_{w_name}"
-
-        wallet = Wallet(name=w_name, unique_name=w_unique_name, owner=user)
-        wallet.save()
-
-        for c in counterparties:
-            c_name = c.upper()
-            c_unique_name = f"{user.username}_{c_name}"
-            counterparty = Counterparty(name=c_name, unique_name=c_unique_name, owner=user)
-            counterparty.save()
-
-        for c in categories:
-            c_name = c.upper()
-            c_unique_name = f"{user.username}_{c_name}"
-            category = Category(name=c_name, unique_name=c_unique_name, owner=user)
-            category.save()
-
-        starting_date = datetime(year=2022, month=1, day=1).date()
-        _category = Category.objects.filter(owner=user)
-        _counterparty = Counterparty.objects.filter(owner=user)
-        _wallet = Wallet.objects.filter(owner=user)
-
-        for i in range(get_days_till_today(starting_date)):
-
-            today = starting_date + timedelta(days=i)
-
-            if today.day == 10:
-                transaction = Transaction(
-                    counterparty=Counterparty.objects.get(name="PRACA"),
-                    category=Category.objects.get(name="PRACA"),
-                    is_profit=True,
-                    value=6000,
-                    date=today,
-                    owner=user,
-                )
-                transaction.save()
-                transaction.wallet.add(choice(_wallet))
-                transaction.save()
-
-            trans_no = randint(0, 3)
-            if not trans_no:
-                continue
-
-            for t in range(trans_no):
-
-                is_profit = choices([False, True], weights=[9, 1])[0]
-                value = randint(5, 200)
-                if not is_profit:
-                    value = -value
-
-                transaction = Transaction(
-                    counterparty=choice(_counterparty),
-                    category=choice(_category),
-                    is_profit=is_profit,
-                    value=value,
-                    date=today,
-                    owner=user,
-                )
-                transaction.save()
-                transaction.wallet.add(choice(_wallet))
-                transaction.save()
-
-        print(f"User {username} fully created.")
+    print("Created wallets:")
+    create_wallets(user)
+    print(f'=' * 60)
+    print("Created plans:")
+    create_plans(user)
+    print(f'=' * 60)
+    create_transactions(user)
+    transactions = Transaction.objects.all().count()
+    print(f"Created transactions: {transactions}")
+    print(f'=' * 60)
 
 
 def say_hello():
